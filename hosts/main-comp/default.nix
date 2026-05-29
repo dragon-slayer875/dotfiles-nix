a49281ff77967a797e0d849254b7f303c64ff949
Rudraksh Tyagi <rudraksh875@gmail.com>
2026-05-29 18:59:44 +0530

    nixos: limit number of configurations retained to 10
    Signed-off-by: Rudraksh Tyagi <rudraksh875@gmail.com>

--- hosts/main-comp/default.nix
+++ hosts/main-comp/default.nix
@@ -27,7 +27,10 @@ in
 
   # Bootloader.
   boot.loader = {
-    systemd-boot.enable = true;
+    systemd-boot = {
+      enable = true;
+      configurationLimit = 10;
+    };
     efi.canTouchEfiVariables = true;
   };
 
