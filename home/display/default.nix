a0ab191dae0afc128dc4f1751ff5097db906ba54
Rudraksh Tyagi <rudraksh875@gmail.com>
2026-06-02 01:02:07 +0530

    nixos: add app and options for gtk theming
    Signed-off-by: Rudraksh Tyagi <rudraksh875@gmail.com>

--- home/display/default.nix
+++ home/display/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   # set cursor size and dpi for 4k monitor
   xresources.properties = {
@@ -6,10 +6,14 @@
     "Xft.dpi" = 172;
   };
 
+  dconf = {
+    enable = true;
+    settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+  };
+
   home.packages = with pkgs; [
     nwg-look
     qt6Packages.qt6ct
-    adw-gtk3
     (catppuccin-gtk.override {
       variant = "macchiato";
       accents = [
@@ -27,9 +31,31 @@
   gtk = {
     enable = true;
 
+    # this theme is needed to use noctalia's color scheme in gtk apps
+    theme = {
+      name = "adw-gtk3-dark";
+      package = pkgs.adw-gtk3;
+    };
+
+    colorScheme = "dark";
+
     iconTheme = {
       name = "Papirus-Dark";
       package = pkgs.papirus-icon-theme;
+    };
+  };
+
+  xdg.configFile = {
+    "gtk-4.0/assets" = {
+      source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
+    };
+
+    "gtk-4.0/gtk.css" = {
+      source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
+    };
+
+    "gtk-4.0/gtk-dark.css" = {
+      source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
     };
   };
 }
