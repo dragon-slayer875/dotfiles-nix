bd93543f754aabc26f7892b2fa38265c0483d060
Rudraksh Tyagi <rudraksh875@gmail.com>
2026-06-20 01:07:58 +0530

    nixos: remove redundant kanata setup from config
    Signed-off-by: Rudraksh Tyagi <rudraksh875@gmail.com>

--- modules/kanata.nix
+++ modules/kanata.nix
@@ -6,14 +6,6 @@
   # Enable uinput
   hardware.uinput.enable = true;
 
-  # Set up udev rules for uinput
-  services.udev.extraRules = ''
-    KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"
-  '';
-
-  # Ensure the uinput group exists
-  users.groups.uinput = { };
-
   # Add the Kanata service user to necessary groups
   systemd.services.kanata-internalKeyboard.serviceConfig = {
     SupplementaryGroups = [
