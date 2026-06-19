1f62cf674027ab45699d84e3222c34ff3eba92d6
Rudraksh Tyagi <rudraksh875@gmail.com>
2026-06-20 01:09:57 +0530

    nixos: update syncthing config
    Signed-off-by: Rudraksh Tyagi <rudraksh875@gmail.com>

--- home/services/default.nix
+++ home/services/default.nix
@@ -1,8 +1,40 @@
-{ ... }:
+{ username, ... }:
 {
   services = {
     syncthing = {
       enable = true;
+      settings = {
+        devices = {
+          "main-phone" = {
+            id = "6IVRSMT-VNGQE7C-XK7L6DW-SFLWYNS-7CNX7BS-ERG2KON-3RYRSK4-7GYDQQ3";
+          };
+        };
+        folders = {
+          "KeePass" = {
+            path = "/home/${username}/Documents/KeePass";
+            devices = [
+              "main-phone"
+            ];
+            versioning = {
+              type = "simple";
+              params.keep = "5";
+              params.cleanoutDays = "7";
+            };
+          };
+
+          "Mihon" = {
+            path = "/home/${username}/Documents/Mihon";
+            devices = [
+              "main-phone"
+            ];
+            versioning = {
+              type = "simple";
+              params.keep = "5";
+              params.cleanoutDays = "7";
+            };
+          };
+        };
+      };
     };
 
     udiskie.enable = true;
